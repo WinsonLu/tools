3f230a2a7d05dabddd45d102b778007d80c6c9a0
WinsonLu <winsonlys@qq.com>
2020-03-11 20:06:22 +0800

    1.扩充部署脚本的功能，添加对ubuntu和archlinux的适配

--- vimrc/setup.sh
+++ vimrc/setup.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-sudo apt install neovim python3 clang-9 python3-pip
-pip3 install jedi pynvim
+if [ $# -ne 1 ]
+then
+	echo "Usage: setup.sh [ubuntu|arch]"
+	exit
+fi
+
+dist=$1
+case $dist in
+	ubuntu)
+		sudo apt install neovim python3 clang-9 python3-pip
+		;;
+	arch)
+		sudo pacman -S neovim python3 clang python-pip
+		;;
+	*)
+		echo "Usage: setup.sh [ubuntu|arch]"
+		exit
+		;;
+esac
+sudo pip3 install jedi pynvim
+
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim
+echo "alias vim='nvim'" >> ~/.zshrc
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
